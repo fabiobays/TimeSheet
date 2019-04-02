b6a7dc44d2f83c919a507b23feac3b4a964de359
fabiobays@gmail.com <fabiobays@gmail.com>
2019-04-01 22:15:46 -0700

    Iniital Commit

---
+++ Project/Payment.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Payment]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [employeeId] INT NOT NULL, 
+    [timesheetMonthId] INT NOT NULL, 
+    [hourlyRate] DECIMAL(18, 2) NOT NULL, 
+    [gross] DECIMAL(18, 2) NOT NULL, 
+    [cpp] DECIMAL(18, 2) NOT NULL, 
+    [ei] DECIMAL(18, 2) NOT NULL, 
+    [net] DECIMAL(18, 2) NOT NULL, 
+    [tax] DECIMAL(18, 2) NOT NULL, 
+    CONSTRAINT [FK_Payment_ToEmployee] FOREIGN KEY (employeeId) REFERENCES Employee(id), 
+    CONSTRAINT [FK_Payment_ToTimeSheetMonth] FOREIGN KEY (timesheetMonthId) REFERENCES TimeSheetMonth(id) 
+)
